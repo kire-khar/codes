d5a038e11e5d6ba8549a1a2b3dc6720bcce04c65
kire-khar <35968517+kire-khar@users.noreply.github.com>
2018-01-30 20:20:04 +0330

    Create P1.cpp

---
+++ P1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int res = 0;
+    for (int i = 1; i < 1000; i ++)
+        if ((i % 3) * (i % 5) == 0)
+            res += i;
+    cout << res << endl;
+}
